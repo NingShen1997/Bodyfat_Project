01c4a19182f82be36d536cba2592c6dde202fe77
H Jiang <30893824+Rev-Jiang@users.noreply.github.com>
2019-02-07 11:12:43 +0800

    Update ShinyAPP.R

--- Codes/ShinyAPP.R
+++ Codes/ShinyAPP.R
@@ -21,9 +21,6 @@ ui <- fluidPage(
     div(style="display: inline-block;vertical-align:top; width: 70px;",HTML("<br>")),
     div(style="display: inline-block;vertical-align:top; width: 190px;",selectInput(inputId = "abdomenMeasure", label=h3("cm/inches"), choices=c("cm","inches"),selected="cm")),
     
-    
-   
-    
     width=7
   ),
   
